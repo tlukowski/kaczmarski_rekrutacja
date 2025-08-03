ef198821aa12ba12a12f920c090739d37d133f50
tlukowski <tomeklukowski@gmail.com>
2025-08-03 15:49:43 +0200

    fix skeleton for mobile animation

--- src/components/ui/skeleton/skeleton.tsx
+++ src/components/ui/skeleton/skeleton.tsx
@@ -5,41 +5,43 @@ interface SkeletonProps {
 }
 
 const Skeleton = ({ rowNumber }: SkeletonProps) => (
-  <table className="skeleton">
-    <thead>
-      <tr className="skeleton__row">
-        <th className="skeleton__cell">
-          <div className="skeleton__line"></div>
-        </th>
-        <th className="skeleton__cell">
-          <div className="skeleton__line"></div>
-        </th>
-        <th className="skeleton__cell">
-          <div className="skeleton__line"></div>
-        </th>
-        <th className="skeleton__cell">
-          <div className="skeleton__line"></div>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      {Array.from({ length: rowNumber }, (_, index) => (
-        <tr key={index}>
-          <td className="skeleton__cell">
+  <div className="skeleton__table-container">
+    <table className="skeleton__table">
+      <thead>
+        <tr className="skeleton__row">
+          <th className="skeleton__cell">
             <div className="skeleton__line"></div>
-          </td>
-          <td className="skeleton__cell">
+          </th>
+          <th className="skeleton__cell">
             <div className="skeleton__line"></div>
-          </td>
-          <td className="skeleton__cell">
+          </th>
+          <th className="skeleton__cell">
             <div className="skeleton__line"></div>
-          </td>
-          <td className="skeleton__cell">
+          </th>
+          <th className="skeleton__cell">
             <div className="skeleton__line"></div>
-          </td>
+          </th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {Array.from({ length: rowNumber }, (_, index) => (
+          <tr key={index}>
+            <td className="skeleton__cell">
+              <div className="skeleton__line"></div>
+            </td>
+            <td className="skeleton__cell">
+              <div className="skeleton__line"></div>
+            </td>
+            <td className="skeleton__cell">
+              <div className="skeleton__line"></div>
+            </td>
+            <td className="skeleton__cell">
+              <div className="skeleton__line"></div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 export default Skeleton;
